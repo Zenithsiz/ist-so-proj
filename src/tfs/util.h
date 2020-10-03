f1f80851c9d8312e87a360f5ed8891ac0aed63a0
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2020-10-03 02:49:56 +0100

    Fixed working in `tfs/util`

--- src/tfs/util.h
+++ src/tfs/util.h
@@ -6,7 +6,7 @@
 
 /// @brief Returns the minimum value between `lhs` and `rhs`
 /// @details
-/// Inspired by https://stackoverflow.com/questions/3437404/min-and-max-in-c
+/// Adapted from https://stackoverflow.com/questions/3437404/min-and-max-in-c
 #define TFS_MIN(lhs, rhs)          \
 	({                             \
 		typeof(lhs) _lhs = (lhs);  \
@@ -16,7 +16,7 @@
 
 /// @brief Returns the maximum value between `lhs` and `rhs`
 /// @details
-/// Inspired by https://stackoverflow.com/questions/3437404/min-and-max-in-c
+/// Adapted from https://stackoverflow.com/questions/3437404/min-and-max-in-c
 #define TFS_MAX(lhs, rhs)          \
 	({                             \
 		typeof(lhs) _lhs = (lhs);  \
