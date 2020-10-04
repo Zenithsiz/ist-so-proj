fc3b7082300b938c2f76e5385f05f64f5f0a0b95
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2020-10-04 09:47:31 +0100

    Revised binary `tecnicofs`.

--- src/bin/tecnicofs.c
+++ src/bin/tecnicofs.c
@@ -5,56 +5,72 @@
 #include <string.h>
 #include <tfs/fs.h>
 
-#define MAX_INPUT_SIZE 100
-
+/// @brief Applies commands from stdin
 static void apply_commands(TfsFs* fs) {
-	char line[MAX_INPUT_SIZE];
+	// Each command has at most `[command] [name] {type}\n`
+	// `command` / `type`: 1 character,
+	// `name`: TFS_DIR_MAX_FILE_NAME_LEN characters + 1 null.
+	char line[TFS_DIR_MAX_FILE_NAME_LEN + 6];
 
 	while (fgets(line, sizeof(line), stdin)) {
-		char token, type;
-		char name[MAX_INPUT_SIZE];
-
-		int numTokens = sscanf(line, "%c %s %c", &token, name, &type);
-		if (numTokens < 2) {
-			fprintf(stderr, "Error: invalid command\n");
+		// If the line starts with ^D, return
+		if (line[0] == '\x4') {
 			return;
 		}
 
+		// Read at least the command and name
+		char command, type;
+		char name[TFS_DIR_MAX_FILE_NAME_LEN + 1];
+		int tokens_read = sscanf(line, "%c %s %c\n", &command, name, &type);
+		if (tokens_read < 2) {
+			fprintf(stderr, "Usage: [command] [name] {type}\n");
+			continue;
+		}
+
+		// Create a path from the name
 		TfsPath path = tfs_path_from_cstr(name);
 
-		switch (token) {
-			case 'c':
+		// And check the command
+		switch (command) {
+			// Create path
+			case 'c': {
+				// If we didn't get a type, print err and continue
+				if (tokens_read != 3) {
+					fprintf(stderr, "Usage: c [name] [type]\n");
+					continue;
+				}
+
+				// Get the type of what we're creating
+				TfsInodeType inode_type;
 				switch (type) {
 					case 'f': {
-						printf("Creating file %s\n", name);
-						TfsFsCreateResult res = tfs_fs_create(fs, TfsInodeTypeFile, path);
-						if (res.kind != TfsFsCreateResultSuccess) {
-							printf("Unable to create file '%s'\n", name);
-							tfs_fs_create_result_print(&res, stdout);
-						}
-						else {
-							printf("Successfully created '%s'\n", name);
-						}
+						inode_type = TfsInodeTypeFile;
 						break;
 					}
 					case 'd': {
-						printf("Creating directory '%s'\n", name);
-						TfsFsCreateResult res = tfs_fs_create(fs, TfsInodeTypeDir, path);
-						if (res.kind != TfsFsCreateResultSuccess) {
-							printf("Unable to create directory '%s'\n", name);
-							tfs_fs_create_result_print(&res, stdout);
-						}
-						else {
-							printf("Successfully created '%s' (with index %zu)\n", name, res.data.success.idx);
-						}
+						inode_type = TfsInodeTypeDir;
 						break;
 					}
 					default: {
-						fprintf(stderr, "Error: invalid inode type: '%c'\n", type);
+						fprintf(stderr, "Invalid inode type: '%c'\n", type);
 						continue;
 					}
 				}
+
+				printf("Creating %s %s\n", tfs_inode_type_str(inode_type), name);
+				TfsFsCreateResult res = tfs_fs_create(fs, inode_type, path);
+				if (res.kind != TfsFsCreateResultSuccess) {
+					printf("Unable to create %s '%s'\n", tfs_inode_type_str(inode_type), name);
+					tfs_fs_create_result_print(&res, stdout);
+				}
+				else {
+					printf("Successfully created '%s'\n", name);
+				}
+
 				break;
+			}
+
+			// Look up path
 			case 'l': {
 				TfsFsFindResult res = tfs_fs_find(fs, path);
 				if (res.kind != TfsFsFindResultSuccess) {
@@ -66,6 +82,8 @@ static void apply_commands(TfsFs* fs) {
 				}
 				break;
 			}
+
+			// Delete path
 			case 'd': {
 				printf("Delete: %s\n", name);
 				TfsFsRemoveResult res = tfs_fs_remove(fs, path);
@@ -79,25 +97,24 @@ static void apply_commands(TfsFs* fs) {
 				break;
 			}
 			default: {
-				fprintf(stderr, "Unknown command: '%c'\n", token);
+				fprintf(stderr, "Unknown command: '%c'\n", command);
 				break;
 			}
 		}
 	}
 }
 
-int main(int argc, char* argv[]) {
-	(void)argc;
-	(void)argv;
-
-	/* init filesystem */
+int main(void) {
+	// Create the file system
 	TfsFs fs = tfs_fs_new();
 
-	/* process input and print tree */
+	// Process all input
 	apply_commands(&fs);
+
+	// Print the tree before exiting
 	tfs_fs_print(&fs, stdout);
 
-	/* release allocated memory */
+	// And destroy the file system.
 	tfs_fs_destroy(&fs);
 
 	return EXIT_SUCCESS;
